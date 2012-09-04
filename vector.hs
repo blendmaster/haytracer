96cf5ffdc585487f4d593388162d3da80f69acf8
Steven Ruppert <blendmaster@gmail.com>
2012-09-04 16:23:39 -0600

    Add vector type and functions
    oh gosh I am bad as haskell

---
+++ vector.hs
@@ -0,0 +1,42 @@
+{-
+3d vector functions
+Author: Steven Ruppert
+For CSCI441: Computer Graphics, Fall 2012
+-}
+
+type Scalar = Double -- should it be Num or something?
+
+data Vector = Vector Scalar Scalar Scalar deriving (Show, Eq, Ord)
+
+{-instance Num Vector where-}
+  {-(Vector a1 a2 a3) + (Vector b1 b2 b3) = Vector (a1 + b1)-}
+                                                 {-(a2 + b2)-}
+                                                 {-(a3 + b3)-}
+
+  {-(Vector a1 a2 a3) - (Vector b1 b2 b3) = Vector (a1 - b1)-}
+                                                 {-(a2 - b2)-}
+                                                 {-(a3 - b3)-}
+
+  {-(Vector a1 a2 a3) * (Vector b1 b2 b3) = Vector (a1 * b1)-}
+                                                 {-(a2 * b2)-}
+                                                 {-(a3 * b3)-}
+
+  {-negate (Vector a1 a2 a3) = Vector (negate a1) (negate a2) (negate a3)-}
+
+  {-abs (Vector a1 a2 a3) = Vector (abs a1) (abs a2) (abs a3)-}
+
+
+-- dot product
+(Vector a1 a2 a3) `dot` (Vector b1 b2 b3) = a1 * b1 + a2 * b2 + a3 * b3
+
+-- cross product
+(Vector a1 a2 a3) `cross` (Vector b1 b2 b3) = Vector (a2 * b3 - a3 * b2)
+                                                     (a3 * b1 - a1 * b3)
+                                                     (a1 * b2 - a2 * b1)
+
+magnitude (Vector a1 a2 a3) = sqrt (a1 ** 2 +  a2 ** 2 + a3 ** 2)
+
+normal v@(Vector a1 a2 a3) = Vector (a1 / m) (a2 / m) (a3 / m)
+                             where m = magnitude v
+
+
