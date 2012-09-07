bc719d9fe8dbd468dc4547d432d6672a3db91f98
Steven Ruppert <blendmaster@gmail.com>
2012-09-06 21:36:25 -0600

    Implement pixel scan of world
    As well as first_intersection and stuff. Got ahead of myself whoops

--- World.hs
+++ World.hs
@@ -5,13 +5,12 @@ For CSCI441: Computer Graphics, Fall 2012
 -}
 
 import Vector
+import Data.List
 
-data Material = Material { diffuse_red :: Scalar
-                         , diffuse_green :: Scalar
-                         , diffuse_blue :: Scalar
-                         , ambient_red :: Scalar
-                         , ambient_green :: Scalar
-                         , ambient_blue :: Scalar
+data Color = Color { red, blue, green :: Scalar } deriving (Show, Eq, Ord)
+
+data Material = Material { diffuse :: Color
+                         , ambient :: Color
                          , specular_coef :: Scalar
                          , specular_exp :: Scalar
                          }
@@ -49,8 +48,9 @@ data Ray = Ray { origin :: Point
                }
                deriving (Show, Eq, Ord)
 
--- The closest intersection point between the ray and the primitive
-intersection :: Ray -> Primitive -> Maybe Point
+-- The closest intersection point between the ray and the primitive,
+-- as well as the distance from the ray origin
+intersection :: Ray -> Primitive -> Maybe (Point, Scalar)
 intersection (Ray o d) (Sphere center r _) =
   let a = d `dot` d
       b = (d ^* 2) `dot` (o - center)
@@ -58,7 +58,7 @@ intersection (Ray o d) (Sphere center r _) =
 
       det = b ** 2 - 4 * a * c
 
-      point_from t = Just (o + d ^* t) -- convenience for Maybe constructor
+      point_from t = Just ((o + d ^* t), t)
       choose_from t1 t2
         | t1 < 0 && t2 < 0 = Nothing       -- sphere is 'behind' ray
         | t1 > 0 && t2 < 0 = point_from t1 -- ray starts inside sphere
@@ -78,17 +78,46 @@ intersection (Ray o d) (Triangle v1 v2 v3 _) =
   in if ((v2 - v1) `cross` (p - v1)) `dot` n >= 0 &&
         ((v3 - v2) `cross` (p - v2)) `dot` n >= 0 &&
         ((v1 - v3) `cross` (p - v3)) `dot` n >= 0
-     then Just p
+     then Just (p, t)
      else Nothing
 
--- the closest intersection (to the origin) between a primitive in the world
--- and a ray, or Nothing if there is no such intersection
---
--- oh gosh so bad
-{-first_intersection :: Ray -> World -> Maybe Primitive-}
-{-first_intersection ray world =-}
-  {-let intersections = filter (\p -> (intersection ray p) /= Nothing)-}
-                             {-(primitives world)-}
-  {-in case intersections of [] -> Nothing-}
-                           {-xs -> closest ray xs-}
-  {-where closest ray xs = first (sort (\p -> (magnitude (intersection ray p)))-}
+-- The first shape and the point at which a given ray intersects a list of
+-- primitives, or Nothing if there is no intersection
+-- TODO I sure wish I understood monads better
+first_intersection :: Ray -> [Primitive] -> Maybe (Primitive, Point)
+first_intersection ray primitives =
+  let intersections = map (\s -> (s, (intersection ray s))) primitives
+      just = filter ((/= Nothing) . snd) intersections
+  in case just of
+     [] -> Nothing
+     _  -> let dist (s, Just (p, t)) = t
+               dist_of a b = (dist a) `compare` (dist b)
+               (s, Just (p, t)) = (minimumBy dist_of just)
+           in Just (s, p)
+
+-- The ray point from the viewpoint to the center of the x,yth pixel of
+-- the width x height pixels of the rendered view
+pixel_ray :: World -> Int -> Int -> Int -> Int -> Ray
+pixel_ray world width height x y =
+  let e = viewpoint world
+      l = screen_origin world
+      v = screen_vertical world
+      h = screen_horizontal world
+      fi = fromIntegral
+  in Ray { origin    = e
+         , direction = ((l + h ^* (((fi x) + 0.5) / (fi width))) +
+                        (l + v ^* (((fi y) + 0.5) / (fi height)))) - e
+         }
+
+-- The pixels of a World rendered into width x height pixels
+-- Simply uses the ambient color of the material of the primitive
+-- TODO lighting and shading and stuff
+pixels :: World -> Int -> Int -> [[Color]]
+pixels world width height =
+  [[ let ray = pixel_ray world width height x y
+     in case first_intersection ray (primitives world) of
+       Nothing        -> Color 0 0 0 -- background
+       Just (prim, _) -> ambient . material $ prim
+  | y <- [0..(height - 1)]]
+  | x <- [0..(width - 1)]]
+
