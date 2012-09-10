1224b4c1faea3fec9d8898113e2cc142a6db3f22
Steven Ruppert <blendmaster@gmail.com>
2012-09-10 01:54:15 -0600

    Implement input format parser
    With some maaagical pattern matching. That went better than expected.
    
    Something's wrong with my triangle code though, as some triangles just
    aren't showing up. Will fix later.

--- Main.hs
+++ Main.hs
@@ -8,49 +8,59 @@ For CSCI441: Computer Graphics, Fall 2012
 
 import Vector
 import World
+import System.Environment
 
-test = World { viewpoint         = Vector 0 1 40
-             , screen_origin     = Vector (-1) (-1) 1
-             , screen_horizontal = Vector 2 0 0
-             , screen_vertical   = Vector 0 2 0
-             , light_source      = Vector 10 3 2
-             , light_intensity   = 0.8
-             , ambient_intensity = 0.2
-             , primitives = [ Sphere { center = Vector 0.5 0.5 0
-                                     , radius = 0.45
-                                     , material =
-                                         Material { diffuse = Color 1 0 0
-                                                  , ambient = Color 1 0 0
-                                                  , specular_coef = 0
-                                                  , specular_exp = 1 }}
-                            , Sphere { center = Vector 0.5 (-0.5) 0
-                                     , radius = 0.45
-                                     , material =
-                                         Material { diffuse = Color 0 1 0
-                                                  , ambient = Color 0 1 0
-                                                  , specular_coef = 0.4
-                                                  , specular_exp = 1 }}
-                            , Sphere { center = Vector (-0.5) 0.5 0
-                                     , radius = 2
-                                     , material =
-                                         Material { diffuse = Color 0 0 1
-                                                  , ambient = Color 0 0 1
-                                                  , specular_coef = 0
-                                                  , specular_exp = 1 }}
-                            , Sphere { center = Vector (-0.5) (-0.5) 0
-                                     , radius = 2
-                                     , material =
-                                         Material { diffuse = Color 1 1 1
-                                                  , ambient = Color 1 1 1
-                                                  , specular_coef = 0
-                                                  , specular_exp = 1 }}
-                            ,  Sphere { center = Vector 0 0 0
-                                     , radius = 2
-                                     , material =
-                                         Material { diffuse = Color 1 1 0
-                                                  , ambient = Color 1 1 0
-                                                  , specular_coef = 0
-                                                  , specular_exp = 1 }}]}
+-- haskell doesn't accept a leading decimal point e.g. ".02425" with
+-- vanilla Read, so this hacks around it. TODO look up better solution
+parse_double :: String -> Scalar
+parse_double num@('.':digits) = read $ '0':num
+parse_double num = read num
+
+parse_material :: [Scalar] -> Material
+parse_material [kdr, kdg, kdb, kar, kag, kab, ks, nspec] =
+  Material { diffuse       = Color kdr kdg kdb
+           , ambient       = Color kar kag kab
+           , specular_coef = ks
+           , specular_exp  = nspec
+           }
+
+parse_primitives :: [String] -> [Primitive]
+parse_primitives [] = []
+parse_primitives ("S":v) =
+  let (cx:cy:cz:r:m) = map parse_double $ take 12 v
+  in Sphere { center   = Vector cx cy cz
+            , radius   = r
+            , material = parse_material m
+            }
+     : (parse_primitives $ drop 12 v)
+parse_primitives ("T":v) =
+  let (x1:y1:z1:x2:y2:z2:x3:y3:z3:m) = map parse_double $ take 17 v
+  in Triangle { v1       = Vector x1 y1 z1
+              , v2       = Vector x2 y2 z2
+              , v3       = Vector x3 y3 z3
+              , material = parse_material m
+              }
+     : (parse_primitives $ drop 17 v)
+
+parse_world :: [String] -> World
+parse_world input =
+  let [ex,ey,ez
+       ,lx,ly,lz
+       ,hx,hy,hz
+       ,vx,vy,vz
+       ,bx,by,bz,i,a] = map parse_double $ take 17 input
+      prims = drop 18 input -- drop useless "number of primatives" field
+  in World { viewpoint         = Vector ex ey ez
+           , screen_origin     = Vector lx ly lz
+           , screen_horizontal = Vector hx hy hz
+           , screen_vertical   = Vector vx vy vz
+
+           , light_source      = Vector bx by bz
+           , light_intensity   = i
+           , ambient_intensity = a
+
+           , primitives        = parse_primitives prims
+           }
 
 -- the PPM file from a 2d array of color pixels
 ppm :: [[Color]] -> String
@@ -67,6 +77,13 @@ ppm pixels =
     "P3\n" ++ width ++ " " ++ height ++ " 255\n" ++ channels
 
 main = do
-  putStrLn "Writing output..."
-  writeFile "output.ppm" (ppm (pixels test 512 512))
+  (infile:_) <- getArgs
+  putStrLn $ "Reading " ++ infile ++ "..."
+  input <- readFile infile
+  putStrLn "Rendering to output.ppm..."
+  let (w:h:rest) = words input
+  putStrLn $ show rest
+  let world = parse_world rest
+  putStrLn $ "world: \n" ++ show world
+  writeFile "output.ppm" (ppm (pixels world (read w) (read h)))
   putStrLn "done!"
